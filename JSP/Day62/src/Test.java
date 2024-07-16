90382944db13479ddd8f68a30abeaca404ab56b1
HyunJaeGit <130658088+HyunJaeGit@users.noreply.github.com>
2024-07-16 23:30:38 +0900

    Delete JSP/Day62 directory

--- JSP/Day62/src/Test.java
+++
@@ -1,7 +0,0 @@
-import com.oreilly.servlet.MultipartRequest;
-
-public class Test {
-	public static void main(String[] args) {
-		// new MultipartRequest(request, saveDirectory, maxPostSize, encoding)
-	}
-}
